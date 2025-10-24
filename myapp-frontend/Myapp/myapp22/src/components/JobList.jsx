aad5094a347bcf423c4c306141881e65d1d0daf4
allanndunda <allanndunda>
2025-10-24 09:55:03 +0300

    Added descriptive comment to the usage of the code

--- myapp-frontend/Myapp/myapp22/src/components/JobList.jsx
+++ myapp-frontend/Myapp/myapp22/src/components/JobList.jsx
@@ -8,7 +8,7 @@ const JobList = ({ jobs, showMatch = false }) => {
       </div>
     );
   }
-
+{/* Render each job using the JobCard component */}
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
       {jobs.map((job) => (
